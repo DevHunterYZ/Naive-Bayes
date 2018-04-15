d03cc49577bde9c870002d1bdd777b6c0340c289
DevHunter <32465888+DevHunterYZ@users.noreply.github.com>
2018-04-15 23:01:34 +0300

    Update GaussianNB.py

--- GaussianNB.py
+++ GaussianNB.py
@@ -2,15 +2,15 @@
 from sklearn import datasets
 from sklearn import metrics
 from sklearn.naive_bayes import GaussianNB
-# load the iris datasets
+# İris verisetini yükle.
 dataset = datasets.load_iris()
-# fit a Naive Bayes model to the data
+# Verileri bir Naive Bayes modeliyle eğit.
 model = GaussianNB()
 model.fit(dataset.data, dataset.target)
 print(model)
-# make predictions
+# Tahmin yap.
 expected = dataset.target
 predicted = model.predict(dataset.data)
-# summarize the fit of the model
+# Modelin başarısını ölç.
 print(metrics.classification_report(expected, predicted))
 print(metrics.confusion_matrix(expected, predicted))
